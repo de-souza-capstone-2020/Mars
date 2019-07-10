f7243a27fe2e0e1db129d68d4b6cb76959e63caf
mllemango <msmangochan@gmail.com>
2019-07-10 19:14:01 -0400

    basic template

---
+++ src/Welcome/welcome_page.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+        <View style={styles.body}>
+
+          <View style={styles.logo_area}>
+            <Text style={styles.title_font}> Logo </Text>
+          </View>
+
+          <View style={styles.get_started}>
+            <Text style={styles.title_font}> Hi there </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#dbefff',
+    flex: 1,
+  },
+  logo_area: {
+    flex: 1,
+    justifyContent: 'center',
+    borderColor: 'red',
+    // borderWidth: 1,
+  },
+  get_started:{
+    flex: 2,
+    borderColor: 'blue',
+    justifyContent: 'center',
+    borderWidth: 1,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'black',
+  },
+  title_font: {
+    fontSize: 45,
+    color: 'black',
+    textAlign: 'center',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
