432347988825f1ebddd443d72acb236726ddc76d
hmasha <hmasha@uwaterloo.ca>
2019-10-28 22:13:33 -0400

    get rid of android

--- screens/home.js
+++ screens/home.js
@@ -7,28 +7,57 @@ export default class Home extends Component {
     messages: [
       {
           _id: 1,
-          text: 'This is your personal SleepWell bot. Would you like to learn more about me?',
+          text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
           createdAt: new Date(),
           quickReplies: {
               type: 'radio', // or 'checkbox',
+              keepIt: true,
               values: [
                 {
                   title: 'Yes',
                   value: 'yes',
                 },
                 {
-                  title: 'Will this take long?',
+                  title: 'Yes, let me show you with a picture!',
                   value: 'yes_picture',
                 },
                 {
-                  title: 'Not right now',
+                  title: 'Nope. What?',
                   value: 'no',
                 },
               ],
             },
-
-      }
-
+            user: {
+               _id: 2,
+               name: 'React Native',
+            },
+      },
+           {
+               _id: 2,
+               text: 'This is a quick reply. Do you love Gifted Chat? (checkbox)',
+               createdAt: new Date(),
+               quickReplies: {
+                 type: 'checkbox', // or 'radio',
+                 values: [
+                   {
+                     title: 'Yes',
+                     value: 'yes',
+                   },
+                   {
+                     title: 'Yes, let me show you with a picture!',
+                     value: 'yes_picture',
+                   },
+                   {
+                     title: 'Nope. What?',
+                     value: 'no',
+                   },
+                 ],
+               },
+               user: {
+                 _id: 2,
+                 name: 'React Native',
+               },
+           }
                ]
           };
 
