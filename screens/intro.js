e809c1b2bb45ac58d71c537f2667dbcfeadd2c80
mllemango <msmangochan@gmail.com>
2020-01-30 15:15:54 -0500

    textinput for name and year of birth

--- screens/intro.js
+++ screens/intro.js
@@ -1,21 +1,35 @@
 import React, { Fragment, Component } from "react";
-import { SafeAreaView, StyleSheet, View, Text, Button } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, Button, TextInput,} from "react-native";
 import Swiper from 'react-native-swiper';
 
 export default class Intro extends React.Component {
+
+  state = {
+    language: 'JavaScript'
+  }
   render() {
     return (
-      <Swiper style={styles.wrapper} showsButtons={false}>
+      <Swiper style={styles.wrapper} showsButtons={true}>
         <View style={styles.slide1}>
-          <Text style={styles.text}>We suggest you stay with us for 6-8 weeks</Text>
+          <Text style={styles.intro_text}>We suggest you stay with us for 6-8 weeks</Text>
         </View>
         <View style={styles.slide2}>
-          <Text style={styles.text}>Please enter your nickname</Text>
+          <Text style={styles.input_text}>Please enter your nickname</Text>
+          <TextInput
+            style={styles.textBox}
+            autoCapitalize='words'
+          />
         </View>
         <View style={styles.slide3}>
-          <Text style={styles.text}>Please enter your year of birth</Text>
+          <Text style={styles.input_text}>Please enter your year of birth</Text>
+          <TextInput
+            style={styles.textBox}
+            autoCapitalize='words'
+            keyboardType='numeric'
+          />
         </View>
         <View style={styles.slide2}>
+          <Text style={styles.input_text}>You're all set</Text>
           <Button 
             title="Go to chatbot"
             onPress={()=>this.props.navigation.navigate('Home')}
@@ -27,67 +41,48 @@ export default class Intro extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  body: {
-    backgroundColor: "#dbefff",
-    flex: 1
-  },
-  logo_area: {
-    flex: 1,
-    justifyContent: "center",
-    borderColor: "red"
-    // borderWidth: 1,
-  },
-  get_started: {
-    flex: 2,
-    borderColor: "blue",
-    justifyContent: "center"
-    // borderWidth: 1,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: "black"
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: "black"
-  },
-  title_font: {
-    fontSize: 45,
-    color: "black",
-    textAlign: "center"
-  },
-  highlight: {
-    fontWeight: "700"
-  },
-  wrapper: {},
   slide1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#9DD6EB'
+    backgroundColor: '#9DD6EB',
+    padding: 50,
   },
   slide2: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#97CAE5'
+    backgroundColor: '#97CAE5',
+    padding: 50,
   },
   slide3: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#92BBD9'
+    backgroundColor: '#92BBD9',
+    padding: 50,
   },
-  text: {
+  intro_text: {
     color: '#fff',
     fontSize: 30,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    textAlign: "center",
+  },
+  input_text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingBottom: 50,
+    textAlign: 'center',
+  },
+  textBox: {
+    height: 50, 
+    width: 250, 
+    borderColor: 'white', 
+    borderBottomWidth: 1, 
+    fontSize: 30, 
+    color: 'white', 
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 });
