38c38e50448613f04a2464b35d3971c83246ca2f
Hillary Masha <hmasha@uwaterloo.ca>
2020-03-10 12:14:59 -0400

    Displaying sleep time entries

--- screens/chat-screen/setting.js
+++ screens/chat-screen/setting.js
@@ -2,61 +2,87 @@ import React, { Component } from 'react';
 import { AsyncStorage } from "react-native";
 import { Platform, StyleSheet, View, Text, Button, Alert } from 'react-native';
 import { retrieveSleepDiaryData } from "../utils/save-utils";
+import {
+  generic_messages,
+  sleep_diary_messages,
+  generic_tip,
+  sleep_diary_tip,
+  module
+} from "../data/messages";
 import Moment from "moment";
+import SleepDiary from "../sleepDiary";
+import SplashScreen from "../loading";
 
 export default class Setting extends Component {
-diary = () => retrieveSleepDiaryData("02-28-2020")
-  consoleMessage=()=>{
-    alert("clicked");
-    AsyncStorage.getAllKeys()
-    console.log('data is', retrieveSleepDiaryData("02-28-2020")[1]); 
-    //console.log('data is', retrieveSleepDiaryData("02-28-2020")); 
 
-    //test.map((item,i) => console.log(item))
-    console.log("console message button clicked"); 
-
+  state = {
+    isLoading: true,
+    sleepAttemptTime: null,
+    wakeUpTime: null,
   }
 
-  splitDiary = (val) => val.split("T")
+  async componentDidMount() {
+    await this.getData();
+    this.setState({isLoading: false});
+  }
 
   getData = async () => {
-    alert("clicked");
-    console.log('todays date',today)
     try {        
           await AsyncStorage.getItem(today).then(key => {
-            console.log(JSON.parse(key))
-            //console.log(JSON.parse(key).attemptToSleepTime.toString())
-            sleepAttempt = () => JSON.parse(key).attemptToSleepTime
-            //leaveBed = () => JSON.parse(key).attemptToSleepTime.split("T")[1]
-            //diff = leaveBed - sleepattempt
-            sleepAttemptTime =sleepAttempt().split("T")[1].split(".")[0] //Get time
-            //sleepAttemptTime = out.slice(0, -1) //remove Z, last character
-
-            console.log("Attempt to sleep time: ", out)
-
-
+            console.log(JSON.parse(key));
+            sleepAttempt = JSON.parse(key).attemptToSleepTime;
+            wakeUp = JSON.parse(key).wakeUpTime;
+            sleepAttemptTime = sleepAttempt.split("T")[1].split(".")[0]; //Get time
+            wakeUpTime = wakeUp.split("T")[1].split(".")[0]; //Get time
+            this.setState({sleepAttemptTime: sleepAttemptTime });
+            this.setState({wakeUpTime: wakeUpTime});  
           });
-      
-    } catch (error) {
+        } catch (error) {
       Alert.alert("Couldn't load data", error);
     }
-    console.log("Done writing to console");
+    if (this.state.sleepAttemptTime != null){
+      console.log("Attempt to sleep time: ", this.state.sleepAttemptTime);
+     }
   };
+  
+  getSleepInfo = () =>{
+    test = this.getData
+    const {sleepAttemptTime} = this.state;
+    const sleepAttemptTime_2 = this.state.sleepAttemptTime;
+    console.log("test 1",sleepAttemptTime);
+    console.log("test 2",sleepAttemptTime_2);
+  }
+ 
+ /*  sleep_hygiene_tip = () => {
+    const {isLoading, sleepAttemptTime} = this.state;
+    if (sleepAttemptTime != null){
+      console.log('This is the sleep time',this.getData)
+    }
+    else{
+      console.log("faillllllllll")
+    }
+    
+  }
+
+  testgetdata = ()=>{
+    console.log(getData().wakeUpTime_)
+  } */
 
   render() {
+    const {isLoading, sleepAttemptTime} = this.state;
 
+    if (isLoading) {
+      return <SplashScreen />;
+    }
     return (
       <View style={styles.container}>
-       <Button
-            title=" Console Message Example "
-            onPress={this.getData}
-            color="#E91E63"
-          />
+      <Text>{sleepAttemptTime}</Text>
+      <Text>{wakeUpTime}</Text>
       </View>
     );
   }
 }
-const test = () => retrieveSleepDiaryData("02-28-2020")
+
 const today = Moment(new Date()).format("MM-DD-YYYY")
 const styles = StyleSheet.create({
   container: {
